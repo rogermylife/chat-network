84fad92d3c3b1f570f094d508dc036eb27f746b5
RogeRxLaKer <rogermylifeQ@gmail.com>
2018-06-01 17:45:38 +0800

    Application: init

---
+++ application/src/main/java/com/chatnetwork/app/client/Test2.java
@@ -0,0 +1,17 @@
+package com.chatnetwork.app.client;
+
+import org.hyperledger.fabric.sdk.Channel;
+import org.hyperledger.fabric.sdk.ChannelConfiguration;
+import org.hyperledger.fabric.sdk.Enrollment;
+import org.hyperledger.fabric.sdk.Orderer;
+import org.hyperledger.fabric.sdk.Peer;
+import org.hyperledger.fabric.sdk.security.CryptoSuite;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("XDDDD");
+	}
+
+}
