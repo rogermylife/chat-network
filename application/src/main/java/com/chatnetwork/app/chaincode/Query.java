5fdd5676f375f5888f7aef4a38ef9af3a34b334c
RogeRxLaKer <rogermylifeQ@gmail.com>
2018-06-05 22:08:21 +0800

    Application: query done

--- application/src/main/java/com/chatnetwork/app/chaincode/Query.java
+++ application/src/main/java/com/chatnetwork/app/chaincode/Query.java
@@ -1,12 +1,8 @@
 package com.chatnetwork.app.chaincode;
 
-import static java.nio.charset.StandardCharsets.UTF_8;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
